9bedae822d22f60dea9e99979da54e305e4b4b9c
oboada <oboada@griddynamic.com>
2022-10-10 09:12:15 -0500

    add error params

--- enums/errors_enum.go
+++ enums/errors_enum.go
@@ -1,16 +1,16 @@
 package enums
 
 const (
-	ErrorDBDuplicatedKeyMsg          = "error_duplicate_key"
-	ErrorRequestBodyCode             = "error_request_parameter"
-	ErrorUserNameNotEmptyMsg         = "error_username_could_not_be_empty"
-	ErrorUserNameNotEmptyCode        = "error_username_not_empty"
-	ErrorUserNameExistsMsg           = "error_username_rep_already_exists"
-	ErrorUserNameExistsCode          = "error_username_exists"
-	ErrorUserNameNotFoundMsg         = "error_username_rep_not_found"
-	ErrorUserNameNotFoundCode        = "error_username_not_found"
-	ErrorInsertLocationCode          = "error_inserting_location"
-	ErrorGetByUserNameCode           = "error_getting_user_by_username"
-	ErrorUpdateLocationCode          = "error_updating_location"
-	ErrorGetByLatitudeLongitudeRange = "error_getting_location_by_latitude_longitude_range"
+	ErrorDBDuplicatedKeyMsg          = "error duplicate key"
+	ErrorRequestBodyCode             = "error request parameter"
+	ErrorUserNameNotEmptyMsg         = "error username could not be empty"
+	ErrorUserNameNotEmptyCode        = "error username not empty"
+	ErrorUserNameExistsMsg           = "error username already exists %s"
+	ErrorUserNameExistsCode          = "error username exists %s"
+	ErrorUserNameNotFoundMsg         = "error username not found %s"
+	ErrorUserNameNotFoundCode        = "error username not found %s"
+	ErrorInsertLocationCode          = "error inserting location"
+	ErrorGetByUserNameCode           = "error getting user by username"
+	ErrorUpdateLocationCode          = "error updating location"
+	ErrorGetByLatitudeLongitudeRange = "error getting location by latitude longitude range"
 )
