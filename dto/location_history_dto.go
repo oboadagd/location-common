022e8b6046c404e51663b3ce9e3dcc78c831cca9
oboada <oboada@griddynamic.com>
2022-10-10 18:56:48 -0500

    set not unique userName of LocationHistory

--- dto/location_history_dto.go
+++ dto/location_history_dto.go
@@ -5,7 +5,7 @@ import "time"
 type LocationHistory struct {
 	tableName struct{}  `pg:"location_history,alias:locationHistory"`
 	Id        int64     `json:"id" pg:",pk"`
-	UserName  string    `json:"userName"  pg:"username, unique, notnull, alias:userName"`
+	UserName  string    `json:"userName"  pg:"username, notnull, alias:userName"`
 	Latitude  float64   `json:"latitude"  pg:", notnull"`
 	Longitude float64   `json:"longitude" pg:", notnull"`
 	UpdatedAt time.Time `json:"updatedAt" pg:"updated_at, notnull, alias:updatedAt"`
