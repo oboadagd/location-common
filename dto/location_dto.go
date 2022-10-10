ac3b178ad3dd3ae8881b4221dbcc079e163dec85
oboada <oboada@griddynamic.com>
2022-10-10 11:49:06 -0500

    add db constraint for dtos

--- dto/location_dto.go
+++ dto/location_dto.go
@@ -5,8 +5,8 @@ import "time"
 type Location struct {
 	tableName struct{}  `pg:"location,alias:location"`
 	Id        int64     `json:"id" pg:",pk"`
-	UserName  string    `json:"userName" pg:"username, alias:userName"`
-	Latitude  float64   `json:"latitude"`
-	Longitude float64   `json:"longitude"`
-	UpdatedAt time.Time `json:"updatedAt" pg:"updated_at, alias:updatedAt"`
+	UserName  string    `json:"userName" pg:"username, notnull, unique, alias:userName"`
+	Latitude  float64   `json:"latitude" pg:", notnull"`
+	Longitude float64   `json:"longitude" pg:", notnull"`
+	UpdatedAt time.Time `json:"updatedAt" pg:"updated_at, notnull, alias:updatedAt"`
 }
--- dto/location_history_dto.go
+++ dto/location_history_dto.go
@@ -5,9 +5,9 @@ import "time"
 type LocationHistory struct {
 	tableName struct{}  `pg:"location_history,alias:locationHistory"`
 	Id        int64     `json:"id" pg:",pk"`
-	UserName  string    `json:"userName" pg:"username, alias:userName"`
-	Latitude  float64   `json:"latitude"`
-	Longitude float64   `json:"longitude"`
-	UpdatedAt time.Time `json:"updatedAt" pg:"updated_at, alias:updatedAt"`
-	Distance  float64   `json:"distance"`
+	UserName  string    `json:"userName"  pg:"username, unique, notnull, alias:userName"`
+	Latitude  float64   `json:"latitude"  pg:", notnull"`
+	Longitude float64   `json:"longitude" pg:", notnull"`
+	UpdatedAt time.Time `json:"updatedAt" pg:"updated_at, notnull, alias:updatedAt"`
+	Distance  float64   `json:"distance"  pg:", notnull"`
 }
