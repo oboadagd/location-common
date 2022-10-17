c34589b7ed6397c829477ca66a89607504383040
oboada <oboada@griddynamic.com>
2022-10-17 08:15:50 -0500

    adding GetPagesUsersByLocationAndRadiusResponse, documentation comments

--- dto/get_users_location_radius_request.go
+++ dto/get_users_location_radius_request.go
@@ -6,11 +6,11 @@ import (
 
 // GetUsersByLocationAndRadiusRequest is a http request of GetUsersByLocationAndRadius service
 type GetUsersByLocationAndRadiusRequest struct {
-	Latitude   float64 `json:"latitude" validate:"required,min=-90,max=90,maxDecimals"`    // latitude coordinate of username's location. It is required, belongs to range -90 to 90, allows 8 decimal positions
-	Longitude  float64 `json:"longitude" validate:"required,min=-180,max=180,maxDecimals"` // longitude coordinate of username's location. It is required, belongs to range -90 to 90, allows 8 decimal positions
-	Radius     float64 `json:"radius" validate:"required,gt=0"`                            // range radius. It is required, belongs to range (0 to +infinite)
-	Page       uint64  `json:"page" validate:"min=1"`                                      // page number to show up. It belongs to range [1 to +infinite)
-	ItemsLimit uint64  `json:"itemsLimit" validate:"min=1"`                                // quantity of items per page. It belongs to range [1 to +infinite)
+	Latitude   float64 `json:"latitude" validate:"min=-90,max=90,maxDecimals"`    // latitude coordinate of username's location. It belongs to range -90 to 90, allows 8 decimal positions
+	Longitude  float64 `json:"longitude" validate:"min=-180,max=180,maxDecimals"` // longitude coordinate of username's location. It belongs to range -90 to 90, allows 8 decimal positions
+	Radius     float64 `json:"radius" validate:"required,gt=0"`                   // range radius. It is required, belongs to range (0 to +infinite)
+	Page       uint64  `json:"page" validate:"min=1"`                             // page number to show up. It belongs to range [1 to +infinite)
+	ItemsLimit uint64  `json:"itemsLimit" validate:"min=1"`                       // quantity of items per page. It belongs to range [1 to +infinite)
 }
 
 // Validate applies validations specified previously
--- dto/get_users_location_radius_response.go
+++
@@ -1,6 +0,0 @@
-package dto
-
-// GetUsersByLocationAndRadiusResponse is a http response of GetUsersByLocationAndRadius service
-type GetUsersByLocationAndRadiusResponse struct {
-	Users []Location `json:"users"` // username's location list located in a given radius
-}
