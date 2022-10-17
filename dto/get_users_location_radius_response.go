55de203a9158ec57fac48197c1fd09f60dd72c83
oboada <oboada@griddynamic.com>
2022-10-17 08:17:17 -0500

    fix GetUsersByLocationAndRadiusResponse

--- dto/get_users_location_radius_response.go
+++ dto/get_users_location_radius_response.go
@@ -1,7 +1,7 @@
 package dto
 
 // GetPagesUsersByLocationAndRadiusResponse is a http response of GetUsersByLocationAndRadius service
-type GetPagesUsersByLocationAndRadiusResponse struct {
+type GetUsersByLocationAndRadiusResponse struct {
 	Users      []Location `json:"users"`      // username's location list located in a given radius
 	TotalItems uint64     `json:"totalItems"` // total number of items
 	TotalPages uint64     `json:"totalPages"` // total number of pages
