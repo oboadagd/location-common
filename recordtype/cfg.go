8d0efd2d5e90e020aa4522c8ee148cdd64c11637
oboada <oboada@griddynamic.com>
2022-10-19 14:35:46 -0500

    fix GRPC_HOST env variable to Cfg struct

--- recordtype/cfg.go
+++ recordtype/cfg.go
@@ -3,10 +3,11 @@ package recordtype
 // Cfg is the struct type that contains fields that stores the necessary configuration
 // gathered from the environment.
 var Cfg struct {
-	DBUser string `envconfig:"DB_USER" default:"locuser"`    //  database's username
-	DBPass string `envconfig:"DB_PASS" default:"root"`       // database's password
-	DBName string `envconfig:"DB_NAME" default:"locationdb"` // database's name
-	DBHost string `envconfig:"DB_HOST" default:"localhost"`  // host's ip
-	DBPort int    `envconfig:"DB_PORT" default:"5432"`       // host's port
-	Locale string `envconfig:"LOCALE"  default:"en"`         // locale
+	DBUser   string `envconfig:"DB_USER" default:"locuser"`      //  database's username
+	DBPass   string `envconfig:"DB_PASS" default:"root"`         // database's password
+	DBName   string `envconfig:"DB_NAME" default:"locationdb"`   // database's name
+	DBHost   string `envconfig:"DB_HOST" default:"localhost"`    // host's ip
+	DBPort   int    `envconfig:"DB_PORT" default:"5432"`         // host's port
+	Locale   string `envconfig:"LOCALE"  default:"en"`           // locale
+	GRPCHost string `envconfig:"GRPC_HOST"  default:"localhost"` // GRPC server host
 }
